3afc4d87191f5d5a30f2c73d9eb03ac5ff078ee6
j.rodeschini <j.rodeschini@studenti.unibg.it>
2026-01-03 00:25:09 +0100

    add example ssm template

---
+++ examples/example_ssm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  2 23:41:43 2026
+
+@author: jacopo
+"""
+
+import sys
+import os
+import numpy as np
+
+
+# pip install -e geossm
+import geossm
+
+import geossm.ssm
+
+
+# %% Create the State Space Model (SSM)
