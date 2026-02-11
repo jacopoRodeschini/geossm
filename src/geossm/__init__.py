7a52b12a21d044670051fc2a5829c22eff9d041b
j.rodeschini <j.rodeschini@studenti.unibg.it>
2026-02-11 11:37:09 +0000

    Fix import utils 👷

--- src/geossm/__init__.py
+++ src/geossm/__init__.py
@@ -1,8 +1,9 @@
 
 # geossm package
 from .data_preparation import DesignMatrices, data_preparation
+from .utils import block_diag_3D, write, getHardware
 
-__all__ = ['data_preparation', 'DesignMatrices']
+__all__ = ['data_preparation', 'DesignMatrices', 'block_diag_3D', 'write', 'getHardware']
 
 
 __version__ = '0.1.0'
